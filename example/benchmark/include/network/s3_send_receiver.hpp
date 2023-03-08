b4ee4f6b4112ce5b416af6297309bb167d785313
Dominik Durner <durner@cs.tum.edu>
2023-03-08 11:48:05 +0100

    Require S3 to download data and store it into reusable buffers

--- example/benchmark/include/network/s3_send_receiver.hpp
+++ example/benchmark/include/network/s3_send_receiver.hpp
@@ -17,6 +17,7 @@
 #include <aws/core/Aws.h>
 #include <aws/core/client/ClientConfiguration.h>
 #include <aws/core/utils/Outcome.h>
+#include <aws/core/utils/stream/PreallocatedStreamBuf.h>
 #include <aws/s3-crt/S3CrtClient.h>
 #include <aws/s3-crt/model/GetObjectRequest.h>
 #include <aws/s3/S3Client.h>
@@ -45,12 +46,20 @@ class S3SendReceiver {
     typedef TGetObjectOutcome GetObjectOutcome;
     typedef TClientConfiguration ClientConfiguration;
 
-    /// The message
+    /// The message which discards the file
     struct GetObjectRequestMessage {
+        public:
+        /// The request
         GetObjectRequest objectRequest;
+        /// The result data
+        std::unique_ptr<utils::DataVector<uint8_t>> data;
+        /// The response handler
+        std::unique_ptr<GetObjectResponseReceivedHandler> responseHandler;
+        /// The output IO stream for the request (optional)
+        std::unique_ptr<Aws::Utils::Stream::PreallocatedStreamBuf> streamBuf;
 
         /// The constructor
-        GetObjectRequestMessage(GetObjectRequest req) : objectRequest(req) {}
+        GetObjectRequestMessage(GetObjectRequest req) : objectRequest(req), data(), responseHandler(), streamBuf() {}
 
         /// The destructor
         virtual ~GetObjectRequestMessage() = default;
@@ -59,7 +68,7 @@ class S3SendReceiver {
         virtual bool requiresFinish() { return false; }
 
         /// Callback
-        virtual void finish(std::unique_ptr<utils::DataVector<uint8_t>> /*data*/) {}
+        virtual void finish() {}
     };
 
     /// The callback message
@@ -77,8 +86,8 @@ class S3SendReceiver {
         /// Override if callback required
         bool requiresFinish() override { return true; }
 
-        void finish(std::unique_ptr<utils::DataVector<uint8_t>> data) override {
-            callback(move(data));
+        void finish() override {
+            callback(move(GetObjectRequestMessage::data));
         }
     };
 
@@ -89,11 +98,16 @@ class S3SendReceiver {
     utils::UnorderedMap<uintptr_t, std::unique_ptr<utils::DataVector<uint8_t>>> _completions;
     /// The timinings info
     std::vector<utils::TimingHelper>* _timings;
-
+    /// Reuse elements
+    utils::RingBuffer<utils::DataVector<uint8_t>*> _reuse;
     /// The S3 client
     std::unique_ptr<S3Client> _client;
     /// The s3 client config
     std::unique_ptr<ClientConfiguration> _config;
+    /// Current requests in flight
+    std::atomic<uint64_t> _currentSubmissions;
+    /// Total submissions
+    std::atomic<uint64_t> _totalSubmission;
     /// The queue maximum
     uint64_t _concurrentRequests;
     /// Condition variable to stop wasting wait cycles
@@ -105,7 +119,7 @@ class S3SendReceiver {
 
     public:
     /// The constructor
-    S3SendReceiver(uint64_t submissions, uint64_t concurrentRequests, const std::string& region, uint64_t threadsOrThroughput = 1, bool https = false) : _submissions(submissions), _completions(0.2 * submissions), _timings(nullptr), _concurrentRequests(concurrentRequests), _stopDeamon(false)
+    S3SendReceiver(uint64_t submissions, uint64_t concurrentRequests, const std::string& region, uint64_t threadsOrThroughput = 1, bool https = false) : _submissions(submissions), _completions(0.2 * submissions), _timings(nullptr), _reuse(submissions), _currentSubmissions(), _totalSubmission(), _concurrentRequests(concurrentRequests), _stopDeamon(false)
     /// The constructor
     {
         _config = std::make_unique<ClientConfiguration>();
@@ -126,8 +140,21 @@ class S3SendReceiver {
         _client = std::make_unique<S3Client>(*_config.get());
     }
 
+    /// The destructor
+    ~S3SendReceiver() {
+        while (auto val = _reuse.consume<false>()) {
+            if (val.has_value())
+                delete val.value();
+        }
+    }
+
+    /// Reuse message
+    uint64_t reuse(std::unique_ptr<utils::DataVector<uint8_t>> message) {
+        return _reuse.insert<false>(message.release());
+    }
+
     /// Adds a message to the submission queue
-    bool send(GetObjectRequestMessage* msg) {
+    uint64_t send(GetObjectRequestMessage* msg) {
         return _submissions.insert(msg);
     }
 
@@ -136,7 +163,7 @@ class S3SendReceiver {
         auto it = _completions.find(reinterpret_cast<uintptr_t>(msg));
         if (it != _completions.end()) {
             auto res = move(it->second);
-            _completions.erase(it);
+            std::ignore = _completions.erase(it);
             return res;
         } else {
             return nullptr;
@@ -148,7 +175,7 @@ class S3SendReceiver {
         auto it = _completions.find(reinterpret_cast<uintptr_t>(msg));
         if (it != _completions.end()) {
             auto res = move(it->second);
-            _completions.erase(it);
+            std::ignore = _completions.erase(it);
             return res;
         } else {
             return nullptr;
@@ -172,16 +199,10 @@ class S3SendReceiver {
     /// Stops the deamon
     void stop() { _stopDeamon = true; }
 
-    private:
     /// Creates a sending message and waits for result
-    void sendReceive() {
+    void sendReceive(bool oneQueueInvocation = false) {
         // Reset the deamon status
         _stopDeamon = false;
-
-        // Current requests in flight
-        std::atomic<uint64_t> submissions = 0;
-        std::atomic<uint64_t> totalSubmission = 0;
-        std::vector<std::unique_ptr<GetObjectResponseReceivedHandler>> callbacks;
 
         // Send new requests
         auto emplaceNewRequest = [&] {
@@ -191,26 +212,40 @@ class S3SendReceiver {
                     break;
 
                 auto original = val.value();
-                callbacks.push_back(move(fetchObject(original, submissions, totalSubmission)));
 
-                if (submissions >= _concurrentRequests)
+                if (!original->streamBuf) {
+                    if (auto mem = _reuse.consume<false>()) {
+                        if (mem.has_value()) {
+                            original->data = std::unique_ptr<utils::DataVector<uint8_t>>(mem.value());
+                            original->streamBuf = std::make_unique<Aws::Utils::Stream::PreallocatedStreamBuf>(reinterpret_cast<unsigned char*>(original->data->data()), original->data->capacity());
+                            original->objectRequest.SetResponseStreamFactory([original]() { return Aws::New<Aws::IOStream>("", original->streamBuf.get()); });
+                        }
+                    }
+                }
+
+                original->responseHandler = move(fetchObject(original, _currentSubmissions, _totalSubmission));
+
+                if (_currentSubmissions >= _concurrentRequests)
                     break;
             }
         };
 
         // iterate over all messages
         while (!_stopDeamon) {
-            if (submissions < _concurrentRequests) {
+            if (_currentSubmissions < _concurrentRequests) {
                 emplaceNewRequest();
             } else {
                 std::unique_lock<std::mutex> lock(_mutex);
                 _cv.wait(lock);
             }
+
+            if (oneQueueInvocation && !_currentSubmissions) {
+                _stopDeamon = true;
+            }
         }
     }
 
     /// Fetches S3 Object async
-    /// TODO: this is for benchmarking only, the result is actually discarded
     /// Requires memcpy from outcome.GetResult() to the data vector of req->finish() and _completions
     std::unique_ptr<GetObjectResponseReceivedHandler> fetchObject(GetObjectRequestMessage* req, std::atomic<uint64_t>& submissions, std::atomic<uint64_t>& totalSubmission) {
         uint64_t submissionId = 0;
@@ -221,21 +256,49 @@ class S3SendReceiver {
 
         auto callback = std::make_unique<GetObjectResponseReceivedHandler>([&submissions, submissionId, req, this](const S3Client*, const GetObjectRequest&, const GetObjectOutcome& outcome, const std::shared_ptr<const Aws::Client::AsyncCallerContext>&) {
             if (outcome.IsSuccess()) {
-                submissions--;
+                outcome.GetResult();
+                req->data->resize(outcome.GetResult().GetContentLength());
+                // https://github.com/aws/aws-sdk-cpp/issues/44
                 if (req->requiresFinish())
-                    req->finish(std::make_unique<utils::DataVector<uint8_t>>());
+                    req->finish();
                 else
-                    _completions.push(reinterpret_cast<uintptr_t>(&req), std::make_unique<utils::DataVector<uint8_t>>());
+                    _completions.push(reinterpret_cast<uintptr_t>(&req), move(req->data));
                 _cv.notify_all();
                 if (_timings) {
                     (*_timings)[submissionId].size = outcome.GetResult().GetContentLength();
                     (*_timings)[submissionId].finish = std::chrono::steady_clock::now();
                 }
+                submissions--;
             }
         });
         submissions++;
         _client->GetObjectAsync(req->objectRequest, *callback.get());
         return callback;
+    }
+
+    /// Fetches S3 Object sync
+    void getObjectSync(GetObjectRequestMessage* req, std::atomic<uint64_t>& submissions, std::atomic<uint64_t>& totalSubmission) {
+        uint64_t submissionId = 0;
+        if (_timings) {
+            submissionId = totalSubmission.fetch_add(1);
+            (*_timings)[submissionId].start = std::chrono::steady_clock::now();
+        }
+        submissions++;
+        auto outcome = _client->GetObject(req->objectRequest);
+        if (outcome.IsSuccess()) {
+            req->data->resize(outcome.GetResult().GetContentLength());
+            // https://github.com/aws/aws-sdk-cpp/issues/44
+            if (req->requiresFinish())
+                req->finish();
+            else
+                _completions.push(reinterpret_cast<uintptr_t>(&req), move(req->data));
+            submissions--;
+            _cv.notify_all();
+            if (_timings) {
+                (*_timings)[submissionId].size = outcome.GetResult().GetContentLength();
+                (*_timings)[submissionId].finish = std::chrono::steady_clock::now();
+            }
+        }
     }
 };
 //---------------------------------------------------------------------------
